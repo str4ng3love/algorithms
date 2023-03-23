f29d592aeab1662fbcee33fc2d4ba2aa070766c6
str4ng3love <normalemailadress@gmail.com>
2023-03-23 09:11:32 +0100

    fixed linked_list.js

--- linked_list.js
+++ linked_list.js
@@ -8,6 +8,7 @@ class Node {
     return `Node data: ${this.data}`;
   }
 }
+
 class LinkedList {
   constructor() {
     this.head = null;
@@ -67,7 +68,10 @@ class LinkedList {
     return null;
   }
   removeHead() {
-    return (this.head = this.head.nextNode);
+    let oldHead = this.head;
+    this.head = this.head.nextNode;
+    oldHead.nextNode = null;
+    return oldHead;
   }
   removeTail() {
     let current = this.head;
@@ -81,25 +85,65 @@ class LinkedList {
     secondToLast.nextNode = null;
   }
   insert(data, index) {
-    if(index > this.size()){
-        return `Error: Index out of bounds`
+    if (index > this.size()) {
+      return `Error: Index out of bounds`;
     }
-    if( index === 0){
-        this.add(data)
+    if (index === 0) {
+      let oldHead = this.head;
+      let newNode = new Node(data);
+      this.head = newNode;
+      this.head.nextNode = oldHead;
+      return this.explain();
     }
-    if(index > 0){
-        let newNode = new Node(data)
-        let current = this.head;
-        let position = index
-        while(position > 1){
-            current = current.nextNode
-            position -=1
-        }
-        let prev = current
-        let next = current.nextNode
-        prev.nextNode = newNode
-        newNode.nextNode = next
-        return this.explain()
+    if (index > 0) {
+      let newNode = new Node(data);
+      let current = this.head;
+      let position = index;
+      while (position > 1) {
+        current = current.nextNode;
+        position -= 1;
+      }
+      let prev = current;
+      let next = current.nextNode;
+      prev.nextNode = newNode;
+      newNode.nextNode = next;
+      return this.explain();
+    }
+  }
+  erase(index) {
+    console.log("index :", index);
+
+    if (index === undefined || null) {
+      return console.log("No index position specified");
+    } else if (index > this.size() || index < 0) {
+      return console.log("Error: index out of bounds");
+    } else if (index === 0) {
+      return this.removeHead();
+    } else {
+      let current = this.head;
+      let position = 0;
+      let beforeErased;
+      while (position < index) {
+        beforeErased = current;
+        current = current.nextNode;
+        position += 1;
+      }
+
+      let toBeErased = current;
+      let afterErased = toBeErased.nextNode;
+      console.log(beforeErased)
+      console.log(toBeErased)
+      console.log(afterErased)
+      beforeErased.nextNode = afterErased;
+      return toBeErased;
     }
   }
 }
+
+let n1 = new Node(1);
+let l = new LinkedList();
+l.head = n1;
+l.append(2);
+l.append(3);
+l.append(4);
+l.append(5);
